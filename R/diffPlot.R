4611f34a105460c38970b4020845f9121f53a31c
James Opzoomer <jamie@cascade9.home>
2020-04-21 12:00:28 +0100

    Inf bug fix diffPlot

--- R/diffPlot.R
+++ R/diffPlot.R
@@ -1,21 +1,27 @@
 #' @export
 diffPlot <- function(
   pval_df,
-  p_val = "padj", 
+  p_val = "padj",
   threshold = 0.1
 ){
-  
+
+  # Extract plot data
+  plotdf <- data.frame(logfc = pval_df$logfc, pvalue = pval_df[,p_val], cluster = pval_df$cluster)
+
+  # Remove INF rows
+  plotdf <- plotdf[!is.infinite(plotdf$logfc),]
+
   # Create volcano plot
-  gg_logFC = ggplot(pval_df, aes(x=logfc, y=-log(pval_df[,p_val]))) +
-    geom_point(aes(color=(-log(pval_df[,p_val])>-log(threshold)), size=1.3)) +
+  gg_logFC = ggplot(plotdf, aes(x=logfc, y=-log(pvalue))) +
+    geom_point(aes(color=(-log(pvalue)>-log(threshold)), size=1)) +
     theme_bw() +
     scale_color_manual(values = c('grey', 'red2')) +
-    geom_label_repel(data = pval_df,
-                     aes(x=logfc, y=-log(pval_df[,p_val]),
-                         label = ifelse(-log(pval_df[,p_val]) > -log(threshold),
-                                        as.character(cluster) ,"")), point.padding = 2) +
+    geom_label_repel(data = plotdf,
+                     aes(x=logfc, y=-log(pvalue),
+                         label = ifelse(-log(pvalue) > -log(threshold),
+                                        as.character(cluster) ,"")), point.padding = 2, force = 10) +
     xlab("LogFC") + ylab("-log(FDR)") + theme(legend.position = "none")
-  
+
   return(gg_logFC)
-  
-} 
+
+}
