c54d4c79d86df3bb8b3faddacf95cfc3bead4d5c
JamesOpz <james.opzoomer@kcl.ac.uk>
2020-09-16 11:20:37 +0100

    bug fix

--- R/subsetSCE.R
+++ R/subsetSCE.R
@@ -1,36 +1,33 @@
 #' @export
-subsetSCE = function(x, ...) {
-  
-  # check validity of input arguments
-  stopifnot(is(x, "SingleCellExperiment"))
-  
-  # construct data.frames of cell & feature metadata
-  md = vapply(x@metadata, as.character, character(ncol(x)))
-  md = data.frame(i = seq_len(ncol(x)), md, 
+subsetSCE = function(sce, ...) {
+
+  # create dataframes of sce metadata
+  md = vapply(sce@metadata, as.character, character(ncol(sce)))
+  md = data.frame(i = seq_len(ncol(sce)), md,
                    check.names = FALSE, stringsAsFactors = FALSE)
-  
-  # filter rows & columns      ...
-  mdf <- try(dplyr::filter(md, ... ), silent = TRUE)
-  if (inherits(mdf, "try-error")) mdf <- md
-  idx <- mdf$i; mdf <- select(mdf, -"i")
-  
+
+  # Subset metadata dataframe
+  mdf = try(dplyr::filter(md, ... ), silent = TRUE)
+  if (inherits(mdf, "try-error")) mdf = md
+  idx = mdf$i; mdf = select(mdf, -"i")
+
   # convert to factors
-  mdf <- mutate_all(mdf, factor)
-  
+  mdf = mutate_all(mdf, factor)
+
   # subset reduced dimensions
-  if (length(reducedDims(x)) > 0) {
-    dr <- reducedDims(x)
-    dr <- lapply(dr, "[", i = idx, j = TRUE)
-  } else dr <- NULL
-  
-  # subset assays & returned filtered SCE
-  as <- lapply(assays(x), "[", i = TRUE, j = idx)
-  x_subset = SingleCellExperiment(assays = as, 
-                       reducedDims = dr)
-  
-  metadata(x_subset) = mdf
-  
-  return(x_subset)
+  if (length(reducedDims(sce)) > 0) {
+    red_dim = reducedDims(sce)
+    red_dim = lapply(red_dim, "[", i = idx, j = TRUE)
+  } else red_dim = NULL
+
+  # subset assays & return filtered SCE
+  asssay = lapply(assays(sce), "[", i = TRUE, j = idx)
+  sce_subset = SingleCellEsceperiment(assays = asssay,
+                       reducedDims = red_dim)
+
+  metadata(sce_subset) = mdf
+
+  return(sce_subset)
 
   }
 
