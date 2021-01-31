d5dd0ee7b4d81adccae9832f23a49ce6f15b9af0
JamesOpz <james.opzoomer@kcl.ac.uk>
2021-01-31 15:58:53 +0000

    added stat test choice

---
+++ R/stat_test_clust.R
@@ -0,0 +1,81 @@
+#' @import broom
+#' @export
+#'
+stat_test_clust <- function(
+  sct,
+  group = 'group',
+  clustering = 'cell_annotation',
+  feature = 'condition',
+  p_val = 'padj',
+  threshold = 0.1,
+  test = "wilcox"
+){
+
+  ## Create the props table
+  counts_table = table(sct@metadata[clustering][,1], sct@metadata[,group])
+
+  props_table = t(t(counts_table) / colSums(counts_table)) * 100
+  counts = as.data.frame.matrix(counts_table)
+  props = as.data.frame.matrix(props_table)
+
+  ggdf = melt(data.frame(cluster = rownames(props), props),
+               id.vars = "cluster", value.name = "proportion", variable.name = "sample_id")
+  ggdf$cluster = factor(ggdf$cluster)
+  ggdf$sample_id = gsub("X", "", ggdf$sample_id)
+
+  # Summarize sampleID to metadata feature
+  feature.summary = data.frame(sample_id = sct@metadata[,group], feature = sct@metadata[,feature]) %>%
+    distinct()
+
+  ## Add condition info
+  mm = match(ggdf$sample_id, feature.summary$sample_id)
+
+
+  # Need to recover the original matrix
+  ggdf$condition = factor(feature.summary$feature[mm])
+
+  # Perform statistical testing
+  pop_split_df = split(ggdf, ggdf$cluster)
+
+  pval_df = data.frame(cluster = NULL, pval = NULL, padj = NULL)
+
+  for(i in 1:length(pop_split_df)){
+
+    population = unique(pop_split_df[[i]]$cluster)
+
+    if(test == "wilcox"){
+      # data = pop_split_df[[i]],
+      p_val = tidy(pairwise.wilcox.test(x = pop_split_df[[i]]$proportion,
+                                   g = pop_split_df[[i]]$condition,
+                                  paired = F, correct = F, exact = F))
+    } else if(test == "t_test"){
+      # data = pop_split_df[[i]],
+      p_val = tidy(pairwise.t.test(x = pop_split_df[[i]]$proportion,
+                                        g = pop_split_df[[i]]$condition,
+                                        paired = F, alternative = "two.sided", pool.sd = F, p.adjust.method = "none"))
+
+    } else{
+      print("Error test not found, please select wither wilcox or t_test")
+      return()
+    }
+
+    ord = order(pop_split_df[[i]]$condition)
+
+    vals = as.data.frame(t(pop_split_df[[i]]$proportion[ord]))
+    colnames(vals) = pop_split_df[[i]]$sample_id[ord]
+
+    means = pop_split_df[[i]][,c("proportion","condition")] %>% group_by(condition) %>% summarize_all(list(mean))
+
+    log_fc = log(means[1,2]/means[2,2])
+
+    # Add mean1, mean2 and LogFC
+    pval_df = rbind(pval_df, data.frame(cluster = population, vals, mean_2 = means[1,2],
+                                        mean_2 = means[2,2], logfc = log_fc[1,1], pval = p_val$p.value))
+
+
+  }
+
+  pval_df$padj = p.adjust(pval_df$pval, method = "BH")
+
+  return(pval_df)
+}
---
+++ R/stat_test_expression.R
@@ -0,0 +1,91 @@
+#' @import broom
+#' @export
+#'
+stat_test_expression <- function(
+  indata,
+  assay = 'scaled',
+  grouping = 'group', # The condensing function
+  feature = 'condition', # The contrast
+  clusterAssign = 'cell_annotation', # The clustering
+  test = "wilcox"
+){
+
+  plotobj <- data.frame(Cluster = indata@metadata[,clusterAssign],
+                        Grouping = indata@metadata[,grouping],
+                        as.data.frame(t(as.matrix(assay(indata)))))
+
+  data.median = plotobj %>% group_by( Grouping, Cluster) %>%
+    summarize_all(list(median))
+
+  ## Melt
+  expr_median_sample_cluster_melt = melt(data.median,
+                                         id.vars = c("Grouping", "Cluster"), value.name = "median_expression",
+                                         variable.name = "antigen")
+
+  ## Rearange so the rows represent clusters and markers
+  expr_median_sample_cluster <- dcast(expr_median_sample_cluster_melt,
+                                      Cluster + antigen ~ Grouping, value.var = "median_expression")
+  rownames(expr_median_sample_cluster) <- paste0(expr_median_sample_cluster$Cluster,
+                                                 "_", expr_median_sample_cluster$antigen)
+
+  # Summarize sampleID to metadata feature
+  feature.summary = data.frame(Grouping = indata@metadata[,grouping], Feature = indata@metadata[,feature]) %>%
+    distinct()
+
+  ## Eliminate cases with zero expression in all samples
+  keep0 <- rowSums(as.data.frame(expr_median_sample_cluster[, as.character(feature.summary$Grouping)]), na.rm = TRUE) > 0
+  expr_median_sample_cluster <- expr_median_sample_cluster[keep0, as.character(feature.summary$Grouping)]
+
+  ## Add condition info
+  mm <- match(colnames(expr_median_sample_cluster), feature.summary$Grouping)
+
+
+  # Need to recover the original matrix
+  grouping_vector <- factor(feature.summary$Feature[mm])
+
+  pval_df = NULL
+
+  for(i in 1:length(rownames(expr_median_sample_cluster))){
+
+    if(test == "wilcox"){
+     pval = tidy(pairwise.wilcox.test(as.numeric(expr_median_sample_cluster[i,]),
+                                      g = grouping_vector,
+                                      paired = F, correct = F, exact = F))
+    } else if(test == "t_test"){
+      pval = tidy(pairwise.t.test(as.numeric(expr_median_sample_cluster[i,]),
+                                       g = grouping_vector,
+                                       paired = F, alternative = "two.sided", pool.sd = F, p.adjust.method = "none"))
+    } else{
+      print("Error test not found, please select wither wilcox or t_test")
+      return()
+    }
+
+    ord = order(grouping_vector)
+
+    vals = cbind(t(expr_median_sample_cluster[i,][ord]), condition = grouping_vector[ord])
+
+    means = as.data.frame(vals) %>% group_by(condition) %>% summarize_all(list(~ mean(., na.rm = TRUE)))
+
+    log_fc = log(means[1,2]/means[2,2])
+
+    out_data = data.frame(mean_2 = means[1,2],
+                          mean_2 = means[2,2], logfc = log_fc[1,1])
+
+   colnames(out_data) = c('mean_1', 'mean_2', 'logfc')
+
+   out_data = cbind(out_data, "p_val" = pval$p.value)
+
+    # Add mean1, mean2 and LogFC
+    pval_df = rbind(pval_df, out_data)
+
+  }
+
+  expression_df = cbind(expr_median_sample_cluster, pval_df)
+  expression_df$padj = p.adjust(expression_df$p_val, method = "BH")
+  expression_df = cbind("cluster" = rownames(expression_df), expression_df)
+  rownames(expression_df) = NULL
+
+  return(expression_df)
+
+
+}
